d072ca205768ab9623cd0854d97f9fc655219f3f
Nejuf <nejuf1@gmail.com>
2014-02-17 16:57:40 -0800

    Fixes missing slash bug for new host

--- maps.php
+++ maps.php
@@ -12,22 +12,22 @@
 <body id="getaround_page">
 	<div id="wrapper">
 	  	<div id="header">
-            <h1><?php require($DOCUMENT_ROOT . "topbanner.html"); ?> <!-- loads Shinkamigoto site banner -->
+            <h1><?php require($DOCUMENT_ROOT . "/topbanner.html"); ?> <!-- loads Shinkamigoto site banner -->
             </h1>
 			<div id="nav">
-            	<?php require($DOCUMENT_ROOT . "navbar.html"); ?> <!-- loads the navigation bar -->
+            	<?php require($DOCUMENT_ROOT . "/navbar.html"); ?> <!-- loads the navigation bar -->
 			</div>
 		</div> <!-- header -->
         <div id="headline">
             <div id="search-news">
-                <?php require($DOCUMENT_ROOT . "sidebar_right.html"); ?> <!-- loads the right side of the page with google ads -->
+                <?php require($DOCUMENT_ROOT . "/sidebar_right.html"); ?> <!-- loads the right side of the page with google ads -->
             </div>
         </div>
 		<div id="body">
         	<p>The Goto Islands consist of 140 islands, with the five main islands being Fukue, Hisaka, Naru, Wakamatsu, and Nakadori.  Shinkamigoto makes up Wakamatsu and Nakadori islands, which are connected by a bridge.
             </p>
             <div id="gmap">
-            	<?php require($DOCUMENT_ROOT . "gmap.html"); ?> <!-- loads the customized google map -->
+            	<?php require($DOCUMENT_ROOT . "/gmap.html"); ?> <!-- loads the customized google map -->
             </div>
 			<div class="clear"></div>
 		</div>
