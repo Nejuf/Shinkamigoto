0d847a3c54c8b374dcf61bc4ca4f69c1a85b86e2
Nejuf <nejuf1@gmail.com>
2019-02-05 23:18:16 -0800

    Fixes loadXMLDoc not working on modern browsers

--- src/loadXMLDoc.js
+++ src/loadXMLDoc.js
@@ -1,16 +1,34 @@
 // JavaScript Document
 
-function loadXMLDoc(dname)
-{
-if (window.XMLHttpRequest)
-  {
-  xhttp=new XMLHttpRequest();
+function loadXMLDoc(url, onLoad) {
+  try {
+    // Prefer XMLHttpRequest when available
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', url, true)
+    xhr.setRequestHeader('Content-Type', 'text/xml')
+    xhr.onload = function(e){
+      var xml = xhr.responseXML;
+      if ( !xml ){ //Parse manually (server probably didn't respond with Content-Type == 'text/xml'
+        parser = new DOMParser();
+        xml = parser.parseFromString(xhr.responseText, "text/xml");
+      }
+      onLoad(xml);
+    };
+    xhr.send();
   }
-else
-  {
-  xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  catch (e) {
+    // XMLHttpRequest not available, fallback on ActiveXObject
+    try {
+      var activex = new ActiveXObject('Microsoft.XMLDOM');
+      activex.async = false;
+      activex.load(url); // synchronous
+
+      onLoad(activex);
+    }
+    catch (e) {
+      // Neither XMLHttpRequest or ActiveXObject are available
+      return undefined;
+    }
   }
-xhttp.open("GET",dname,false);
-xhttp.send();
-return xhttp.responseXML;
-} 
+}
+
